c098e84933ffab6400130b026acf98b56f9ba3e5
KristinaGaiist <kristina.gaiist@yandex.ru>
2018-02-09 09:29:14 +0700

    create sort method in SortServiceBase class

--- src/com/TestTask/SortServiceBase.java
+++ src/com/TestTask/SortServiceBase.java
@@ -6,12 +6,7 @@ abstract public class SortServiceBase<T> {
     abstract protected boolean compare(T a, T b);
 
     public void sort(ArrayList<T> list, boolean asceding) {
-        if (asceding == compare(list.get(0), list.get(1))) {
-            list.add(0, list.get(1));
-            list.remove(2);
-        }
-
-        for (int i = 2; i < list.size(); i++) {
+        for (int i = 1; i < list.size(); i++) {
             for (int j = i - 1; j >= 0; j--) {
                 if (asceding == !compare(list.get(j), list.get(i))) {
                     list.add(j + 1, list.get(i));
